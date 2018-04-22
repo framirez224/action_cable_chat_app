98de049e1eccaf2261ca99ca4cc7d19f2df135fe
Fernando Ramirez <feddo.rami@gmail.com>
2018-04-21 21:56:05 -0700

    Actually use the environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://#{HEROKU_HOSTNAME}/cable"
-  config.action_cable.allowed_request_origins = ["http://#{HEROKU_HOSTNAME}"]
+  config.action_cable.url = "wss://#{ENV['HEROKU_HOSTNAME']}/cable"
+  config.action_cable.allowed_request_origins = ["http://#{ENV['HEROKU_HOSTNAME']}"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
